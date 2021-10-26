635db6e5dc233ef9e5fcb1165e73d67dcb5cc109
patrykrybaczek <rybaczekpatryk@icloud.com>
2021-10-26 23:25:41 +0200

    Input error is being handled, css classes is being added for the user to see what he did wrong

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,6 @@
-const button = document.getElementById('get-started');
-const main = document.getElementsByTagName('main')[0];
-const getStartedHTML = document.getElementById('get-started');
-
-const signUpHTML = `
-    <div id="sign-up">
-        <div id="container">
-            <h2>Let us create your account</h2>
-            <input id="username" type="text" placeholder="Username" autocomplete="off">
-            <input id="pin" type="password" placeholder="PIN">
-        </div>
-        <button id="sign-up">Sign Up</button>
-    </div>
-`;
+let button = document.getElementById('get-started');
+let main = document.getElementsByTagName('main')[0];
+let getStartedHTML = document.getElementById('get-started');
 
 button.addEventListener('click', getStarted);
 
@@ -19,6 +8,41 @@ function getStarted() {
     // Clearing the content of getStarted.
     getStartedHTML.remove();
     // Add sign up html to main.
+    let signUpHTML = `
+    <div id="sign-up">
+        <div id="container">
+            <h2>Let us create your account</h2>
+            <input id="username" type="text" placeholder="Username" autocomplete="off">
+            <input id="pin" type="password" placeholder="PIN">
+        </div>
+        <button id="sign-up" onclick="signIn()">Sign Up</button>
+    </div>
+    `;
     main.innerHTML += signUpHTML;
-    document.title = "Noter - Create account"
+    document.title = "Noter - Create account";
+}
+
+function signIn() {
+    errorContainer = document.getElementById('error-container')
+    errorContainer.innerHTML = '';
+    
+    signUpHTML = document.getElementById('sign-up');
+
+    let username = document.getElementById('username');
+    let pin = document.getElementById('pin');
+
+    username.classList.remove('error-input');
+    pin.classList.remove('error-input');
+
+    let error = `<p class="error">Please fill in credentials</p>`;
+
+    if(!username.value && !pin.value) {
+        username.classList.add('error-input');
+        pin.classList.add('error-input');
+        errorContainer.innerHTML += error;
+    } else {
+        signUpHTML.remove();
+        main.innerHTML += noteHTML;
+        document.title = "Noter - My Notes";
+    }
 }
